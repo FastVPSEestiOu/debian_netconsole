fbd331200b7b7e38a54ae6a9161437f930484d33
Pavel Odintsov <pavel.odintsov@gmail.com>
2014-03-25 18:48:29 +0400

    move to curl

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Install arping"
-apt-get install -y arping
+apt-get install -y arping curl
 
 echo "Download config"
-wget --no-check-certificate https://raw.githubusercontent.com/FastVPSEestiOu/debian_netconsole/master/netconsole_conf -O/etc/default/netconsole
+curl -k https://raw.githubusercontent.com/FastVPSEestiOu/debian_netconsole/master/netconsole_conf -o /etc/default/netconsole
 echo "Download init script"
-wget --no-check-certificate https://raw.githubusercontent.com/FastVPSEestiOu/debian_netconsole/master/netconsole -O/etc/init.d/netconsole
+curl -k https://raw.githubusercontent.com/FastVPSEestiOu/debian_netconsole/master/netconsole -o /etc/init.d/netconsole
 
 chmod +x /etc/init.d/netconsole
 
